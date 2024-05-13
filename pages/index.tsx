08d3d9801226c62a733e18e41bfe307dcc331e46
Protengstar <musthofasifaulq@gmail.com>
2024-05-13 15:57:06 +0700

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-export default function Home() {
+import Nav from "@/Components/Nav";
+import { useState } from "react";
+import MobileNav from "../Components/MobileNav";
+
+const HomePage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
   return (
-    <>
+    <div className="overflow-x-hidden">
       <div>
-        <h1>HomePage</h1>
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default HomePage;
