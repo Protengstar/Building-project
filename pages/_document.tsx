fb850e1990462e02c754b1459feee1d368267f7d
Protengstar <musthofasifaulq@gmail.com>
2024-05-18 23:03:03 +0700

    Update Scroll smooth

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head className="scroll-smooth" style={{ scrollBehavior: "smooth" }} />
+    <Html lang="en" className="scroll-smooth" style={{ scrollBehavior: "smooth" }}>
+      <Head />
       <body className="font-primary">
         <Main />
         <NextScript />
